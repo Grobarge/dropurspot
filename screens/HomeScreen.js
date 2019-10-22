c394f97def550a0770a292d13a4945f2bbe69091
Gaje Robarge <gajer@paulmitchell.edu>
2019-10-22 16:27:46 -0600

    have a basic layout of homepage with category's and featured image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,13 +10,16 @@ import {
   View,
   SafeAreaView,
   TextInput,
-  StatusBar
+  StatusBar,
+  Dimensions
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
-
-
 import { MonoText } from '../components/StyledText';
+
+import Category from './explore/Category';
+
+const { height, width } = Dimensions.get('window');
 
 
 export default class HomeScreen extends Component {
@@ -36,11 +39,31 @@ export default class HomeScreen extends Component {
             <View style={{ flexDirection: 'row', padding: 15, backgroundColor: 'white', marginHorizontal: 20, shadowOffset: { width: 0, height: 0 }, shadowColor: 'black', shadowOpacity: 0.2 }}>
               <Icon name='ios-search' size={20} style={{ marginRight: 10 }} />
               <TextInput
-                placeholder="Try Searching"
+                placeholder="Search for a Spot !"
                 placeholderTextColor="grey"
                 style={{ flex: 1, fontWeight: '700', backgroundColor: 'white' }} />
             </View>
           </View>
+          <ScrollView scrollEventThrottle={16}>
+            <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20 }} >
+              <Text style={{ fontSize: 24, fontWeight: '700', paddingHorizontal: 20 }} >Choose a Category!</Text>
+              <View style={{ height: 130, marginTop: 20 }} >
+                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                  <Category imageUri={require('../assets/images/partyspot.jpg')} name="Party" />
+                  <Category imageUri={require('../assets/images/campingspot.jpg')} name="Camping" />
+                  <Category imageUri={require('../assets/images/gamingspot.jpg')} name="Gaming" />
+                  <Category imageUri={require('../assets/images/studyspot.jpg')} name="Study" />
+                </ScrollView>
+              </View>
+              <View style={{ marginTop: 40, paddingHorizontal: 20, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ fontSize: 24, fontWeight: '700', }}>Intoducing SpotDrop Featured</Text>
+                <Text style={{ fontWeight: '100', marginTop: 10 }}>A new selection of spots that are verified</Text>
+                <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
+                  <Image source={require('../assets/images/concert.jpg')} style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 5, borderColor: '#dddddd' }} />
+                </View>
+              </View>
+            </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     )
